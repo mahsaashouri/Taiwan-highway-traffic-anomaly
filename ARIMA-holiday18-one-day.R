eda36f15627edaf43ae43ba141578bffba0a5d3d
Mahsa Ashouri <mahsa.ashouri@iss.nthu.edu.tw>
2022-12-29 16:20:59 -0800

    Added Sample ARIMA codes for last one day holiday - added olsfc for computing OLS results

---
+++ ARIMA-holiday18-one-day.R
@@ -0,0 +1,309 @@
+
+library(tidyverse)
+library(forecast)
+source('smatrix.R')
+
+## reading data
+hour.all <- read.csv('hour.all.csv', header = TRUE)[,-1]
+
+## Drop highway No.5
+hour.all <- subset(hour.all, Freeway!= 'No5')
+
+## Coding station, freeway and area
+hour.all$Station.code <- as.numeric(as.factor(hour.all$Station))
+hour.all$Freeway.code <- as.numeric(as.factor(hour.all$Freeway))
+hour.all$Area.code <- as.numeric(as.factor(hour.all$Area))
+
+
+## fixing the station and vehicle codes
+hour.all$Station.code <- sprintf("%03d", as.numeric(hour.all$Station.code))
+hour.all$vehicle.type <- sprintf("%02d", as.numeric(hour.all$vehicle.type))
+
+## Creating category column
+hour.all$cat <- paste(hour.all$sensor, hour.all$direction, hour.all$vehicle.type,  hour.all$Station, hour.all$Freeway, hour.all$Area, hour.all$Region)
+
+### removing just zero series
+hour.all <- hour.all %>%
+  group_by(cat) %>%
+  filter(any(sum(traffic != 0)))
+
+### removing 80% zero series
+hour.all <- hour.all %>%
+  group_by(cat) %>%
+  filter(mean(traffic == 0) <= 0.8)
+
+## sorting all the series
+hour.all <- hour.all[order(hour.all$cat),]
+
+## Each series in one column
+hourmatrix <- hour.all$traffic %>%
+  matrix(nrow = 20424, ncol = 1590) %>%
+  as.data.frame() 
+
+## Create columns' name
+names <- unique(paste(substr(hour.all$Region, 1, 1), hour.all$Station.code,  hour.all$Freeway.code, hour.all$direction, hour.all$vehicle.type, sep = "")) 
+colnames(hourmatrix) <- names
+
+## Hierarchy structure
+name_length <- str_length(names)
+grouping_hts <- rbind(
+  #Region
+  str_sub(names, start = name_length - 7, end = name_length - 7),
+  #Station
+  str_sub(names, start = name_length - 7, end = name_length - 4),
+  #Freeway
+  str_sub(names, start = name_length - 3, end = name_length - 3),
+  #Direction
+  str_sub(names, start = name_length - 2, end = name_length - 2),
+  #Vehicle
+  str_sub(names, start = name_length - 1, end = name_length),
+  #Region x Direction
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 2, end = name_length - 2), sep = ""),
+  #Region x Freeway
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 3, end = name_length - 3), sep = ""),
+  #Region x Vehicle
+  paste(str_sub(names, start = name_length - 7, end = name_length - 7), str_sub(names, start = name_length - 1, end = name_length), sep = ""),
+  #Freeway x Direction
+  paste(str_sub(names, start = name_length - 3, end = name_length - 3), str_sub(names, start = name_length - 2, end = name_length - 2), sep = ""),
+  #Freeway x Vehicle
+  paste(str_sub(names, start = name_length - 3, end = name_length - 3), str_sub(names, start = name_length - 1, end = name_length), sep = ""),
+  #Vehicle x Direction
+  paste(str_sub(names, start = name_length - 1, end = name_length), str_sub(names, start = name_length - 2, end = name_length - 2), sep = "")
+)
+
+library(hts)
+hourgts <- gts(hourmatrix, groups = grouping_hts) 
+
+allyhourall <- aggts(hourgts) 
+
+## Only the last set of long holidays - One-day
+holiday.18 <- allyhourall[20065:20424,]
+
+allyholiday.18 <- forecast::msts(holiday.18, seasonal.periods=c(24,24*7))
+
+
+
+## base forecasts & 2000 sample paths 
+n <- 360
+t <- 336
+h <- 24
+
+result.all <- list()
+sample.path <- list()
+error.train.all <- NULL
+start.time <- Sys.time()
+for(j in 1:ncol(allyholiday.18)){
+  result <- NULL
+  sample.test <- NULL
+  error.train <- NULL
+  for(i in 0:((n-t-h)/h)){
+    train <- window(allyholiday.18[,j], start = c(1, ((i*h)+1)), end = c(1, (t + (i*h))))
+    valid <- window(allyholiday.18[,j], start = c(1,( t + (i*h) + 1)), end = c(1, (t + (i*h) +h)))
+    #set.seed(123)
+    f.model <- auto.arima(train,  seasonal.test = "ocsb", xreg=fourier(train, K = c(3,2))) 
+    f.error.train <- as.data.frame(c(train)) - as.data.frame(c(forecast(f.model, xreg=fourier(train, K = c(3,2)))$mean))
+    f.fc.test <- forecast(f.model, xreg=fourier(train,K=c(3,2), h = h))$mean
+    sample.paths <- matrix(NA, ncol = 2000, nrow =  24)
+    for (g in 1:2000) {
+      #set.seed(g)
+      sample.paths[,g] <- simulate(f.model, future = TRUE, nsim=h, xreg=fourier(train,K=c(3,2), h = h))
+    }
+    sample.test <- rbind(sample.test, sample.paths)
+    fc.test <- rbind(fc.test, as.data.frame(c(f.fc.test)))
+    error.train <- rbind(error.train, f.error.train)
+    print(j)
+  }
+  result.all[[length(result.all)+1]] <- fc.test
+  sample.path[[length(sample.path)+1]] <- sample.test
+  error.train.all <- bind_cols(error.train.all, error.train)
+}
+end.time <- Sys.time()
+time.taken.base <- end.time - start.time
+
+#### saving base forecasts results
+
+fc.ARIMA <- NULL
+for(i in 1:length(result.all)){
+  fc.ARIMA <- cbind(fc.ARIMA, result.all[[i]][,1])
+}
+
+## ###
+## reconciling base forecasts and sample paths
+## ###
+
+### reading data in a list (each dataframe is for one series)
+library(rio)
+
+mydataf <- lapply(1:ncol(sample.path[[1]]), function(y) as.data.frame(sapply(sample.path, function(x) x[, y])))
+
+# name the data frames
+names(mydataf) <- 1:length(mydataf)
+
+## if we need to read the error files
+# error.train.all <-  import_list(dir(pattern = ".csv"))
+# error.train.all <- lapply(error.train.all, "[", -1)
+# error.train.all <- error.train.all[order(as.numeric(names(error.train.all)))]
+# 
+# error.train.all1 <- do.call("cbind", error.train.all)
+
+fc.ARIMA.res <- list()
+
+for(i in 0:0){
+  fc.ARIMA.res[[i+1]] <- error.train.all[(1+(312*i)):(312+(312*i)),]
+}
+
+library(Matrix)
+start.time <- Sys.time()
+list.G <- list()
+for(i in 1:length(fc.ARIMA.res)){
+  res <- as.matrix(fc.ARIMA.res[[i]])
+  n <- nrow(res)
+  covm <- crossprod(stats::na.omit(res)) / n
+  tar <- diag(apply(res, 2, compose(crossprod, stats::na.omit))/n)
+  corm <- cov2cor(covm)
+  xs <- scale(res, center = FALSE, scale = sqrt(diag(covm)))
+  xs <- xs[stats::complete.cases(xs),]
+  v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
+  diag(v) <- 0
+  corapn <- cov2cor(tar)
+  d <- (corm - corapn)^2
+  lambda <- sum(v)/sum(d)
+  lambda <- max(min(lambda, 1), 0)
+  W <- lambda * tar + (1 - lambda) * covm
+  gmat <- GmatrixG(hourgts$groups)
+  smatrix <- as.matrix(SmatrixM(gmat))
+  R <- t(smatrix)%*%solve(W)
+  P <- Matrix::solve(R%*%smatrix)%*%R
+  SP <- smatrix%*%P
+  list.G[[length(list.G)+1]] <- SP
+  print(i)
+}
+
+## Redonciling sample paths
+list.sample.rec <- list()
+for(i in 1:length(mydataf)){
+  fit.test <- split(mydataf[[i]], rep(1:1,each=(24)))
+  result.path <- NULL
+  for(j in 1:length(list.G)){
+    fc.rec <- matrix(NA, nrow = 24, ncol = ncol(fc.ARIMA.res[[1]]))
+    for(k in 1:nrow(fit.test[[1]])){
+      f.1 <- matrix(as.numeric(fit.test[[j]][k,]), ncol = 1, nrow = ncol(fit.test[[1]]))
+      fc.rec [k,] <- list.G[[j]] %*% f.1
+    }
+    result.path <- bind_rows(result.path, as.data.frame(fc.rec))
+  }
+  list.sample.rec[[length(list.sample.rec)+1]] <- result.path
+  print(i)
+}
+
+end.time <- Sys.time()
+time.taken.sample.path.rec <- end.time - start.time
+
+# name the data frames
+names(list.sample.rec) <- 1:length(list.sample.rec)
+
+## Computing prediction intervals - 95% and 99%
+mydataf2 <- lapply(1:ncol(list.sample.rec[[1]]), function(y) as.data.frame(sapply(list.sample.rec, function(x) x[, y])))
+
+quan975 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan975[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.975))
+}
+
+quan25 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan25[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.025))
+}
+
+quan95 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan95[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.95))
+}
+
+quan05 <- matrix(NA, ncol = length(mydataf2), nrow = nrow(mydataf2[[1]]))
+
+for(i in 1:length(mydataf2)){
+  quan05[,i] <-  apply(mydataf2[[i]], 1, quantile, probs= c(0.05))
+}
+
+
+
+## if we need to read errors and fc files
+
+# error.train.all <-  import_list(dir(pattern = ".csv"))
+# error.train.all <- lapply(error.train.all, "[", -1)
+# error.train.all <- error.train.all[order(as.numeric(names(error.train.all)))]
+# 
+# error.train.all1 <- do.call("cbind", error.train.all)
+# colnames(error.train.all1) <- colnames(allyhour2021)
+# 
+# fc.train.all <-  import_list(dir(pattern = ".csv"))
+# fc.train.all <- lapply(fc.train.all, "[", -1)
+# fc.train.all <- fc.train.all[order(as.numeric(names(fc.train.all)))]
+# 
+# fc.train.all1 <- do.call("cbind", fc.train.all)
+# colnames(fc.train.all1) <- colnames(allyhour2021)
+
+## Reconciling base forecasts
+colnames(fc.ARIMA) <- colnames(allyhourall)
+
+fit.list <- list()
+for(i in 0:0){
+  fit.list[[i+1]] <- fc.ARIMA[(1+(24*i)):(24+(24*i)),]
+}
+fc.ARIMA.res <- list()
+for(i in 0:0){
+  fc.ARIMA.res[[i+1]] <- error.train.all[(1+(312*i)):(312+(312*i)),]
+}
+
+
+list <- list()
+list.G <- list()
+start.time <- Sys.time()
+for(i in 1:length(fc.ARIMA.res)){
+  res <- as.matrix(fc.ARIMA.res[[i]])
+  n <- nrow(res)
+  covm <- crossprod(stats::na.omit(res)) / n
+  tar <- diag(apply(res, 2, compose(crossprod, stats::na.omit))/n)
+  corm <- cov2cor(covm)
+  xs <- scale(res, center = FALSE, scale = sqrt(diag(covm)))
+  xs <- xs[stats::complete.cases(xs),]
+  v <- (1/(n * (n - 1))) * (crossprod(xs^2) - 1/n * (crossprod(xs))^2)
+  diag(v) <- 0
+  corapn <- cov2cor(tar)
+  d <- (corm - corapn)^2
+  lambda <- sum(v)/sum(d)
+  lambda <- max(min(lambda, 1), 0)
+  W <- lambda * tar + (1 - lambda) * covm
+  gmat <- GmatrixG(hourgts$groups)
+  smatrix <- as.matrix(SmatrixM(gmat))
+  R <- t(smatrix)%*%solve(W)
+  P <- Matrix::solve(R%*%smatrix)%*%R
+  SP <- smatrix%*%P
+  
+  fit.test <- fit.list[[i]]
+  fc.rec <- matrix(NA, nrow = 24, ncol = ncol(allyhourall))
+  for(j in 1:nrow(fit.test)){
+    f.1 <- matrix(as.numeric(fit.test[j,]), ncol = 1, nrow = ncol(fit.test))
+    fc.rec [j,] <- SP %*% f.1
+  }
+  colnames(fc.rec) <- colnames(allyhourall) 
+  list[[length(list)+1]] <- fc.rec
+  list.G[[length(list.G)+1]] <- P
+  print(i)
+}
+
+end.time <- Sys.time()
+time.taken.forecast.rec <- end.time - start.time
+
+result.fit.all.rec <- do.call(rbind.data.frame, list)
+
+
+### print computation times
+time.taken.base
+time.taken.sample.path.rec
+time.taken.forecast.rec
+
---
+++ olsfc.R
@@ -0,0 +1,122 @@
+
+
+olsfc_BPI_F <- function(x, h, maxlag = 0, nolag = NULL , sim = 2000) {
+  # Set up data frame for modelling
+  n <- length(x)
+  modeldata <- data.frame(
+    x = as.numeric(x),
+    trend = seq_along(x),
+    f1s_day = sin(2*pi*seq_along(x)/(24)),
+    f1c_day = cos(2*pi*seq_along(x)/(24)),
+    f2s_day = sin(4*pi*seq_along(x)/(24)),
+    f2c_day = cos(4*pi*seq_along(x)/(24)),
+    f3s_day = sin(6*pi*seq_along(x)/(24)),
+    f3c_day = cos(6*pi*seq_along(x)/(24)),
+    f1s_week= sin(2*pi*seq_along(x)/(7*24)),
+    f1c_week = cos(2*pi*seq_along(x)/(7*24)),
+    f2s_week = sin(4*pi*seq_along(x)/(7*24)),
+    f2c_week = cos(4*pi*seq_along(x)/(7*24))
+  )
+  if(maxlag > 0)
+  {
+    lagnames <- paste0("lag", seq(maxlag))
+    for (i in seq_along(lagnames))
+      modeldata[[lagnames[i]]] <- c(rep(NA, i), x[seq(n - i)])
+  } else {
+    lagnames <- NULL
+  }
+  # Set up formula for linear model
+  if (length(nolag) == 0)
+    nolag <- seq(maxlag) 
+  form <- "x ~ trend + f1s_day + f1c_day + f2s_day + f2c_day + f3s_day + f3c_day + f1s_week + f1c_week + f2s_week + f2c_week"
+  for (i in nolag)
+    form <- paste0(form, " + ", "lag", i)
+  form <- as.formula(form)
+  
+  # Fit model
+  fit <- lm(form, data = modeldata)
+  # Adjust the residuals
+  s.resid1 <- residuals(fit)
+  leverage <- influence(fit)$hat
+  s.resid <- residuals(fit)/sqrt(1-leverage)
+  s.resid <- s.resid - mean(s.resid)
+  # Set up forecast vector
+  fc <- msts(numeric(h),
+             seasonal.periods=c(24,24*7),
+             start = c(2, 97)
+  )
+  lwr <- msts(numeric(h),
+              seasonal.periods=c(24,24*7),
+              start = c(2, 97)
+  )
+  upr <- msts(numeric(h),
+              seasonal.periods=c(24,24*7),
+              start = c(2, 97)
+  )
+  # Set up new data for forecasting
+  trend <- length(x) + seq(h)
+  f1s_day = sin(2*pi*(length(x) + seq(h))/(24))
+  f1c_day = cos(2*pi*(length(x) + seq(h))/(24))
+  f2s_day = sin(4*pi*(length(x) + seq(h))/(24))
+  f2c_day = cos(4*pi*(length(x) + seq(h))/(24))
+  f3s_day = sin(6*pi*(length(x) + seq(h))/(24))
+  f3c_day = cos(6*pi*(length(x) + seq(h))/(24))
+  f1s_week = sin(2*pi*(length(x) + seq(h))/(7*24))
+  f1c_week = cos(2*pi*(length(x) + seq(h))/(7*24))
+  f2s_week = sin(4*pi*(length(x) + seq(h))/(7*24))
+  f2c_week = cos(4*pi*(length(x) + seq(h))/(7*24))
+  newdata <- cbind.data.frame(trend = trend[1], f1s_day = f1s_day[1], f1c_day = f1c_day[1], f2s_day = f2s_day[1], 
+                              f2c_day = f2c_day[1], f3s_day = f3s_day[1], f3c_day = f3c_day[1], f1s_week = f1s_week[1], 
+                              f1c_week = f1c_week[1], f2s_week = f2s_week[1], f2c_week = f2c_week[1])
+  for (i in seq_along(lagnames))
+    newdata[[lagnames[i]]] <- tail(x,i)[1]
+  newdata.frame <- matrix(NA, ncol = ncol(newdata), nrow = h)
+  ep.drawslist <- list()
+  # Compute forecasts
+  for (i in seq_along(fc))
+  {
+    fore <- predict(fit , newdata = newdata)
+    ## PI using bootstrap
+    #ep.draws <- replicate( n = sim, unname(fore + sample(s.resid,size=1)))
+    ep.draws <- fore + sample(s.resid,size=sim, replace = TRUE)
+    PI <- fore + quantile(ep.draws, probs = c(0.025, 0.975))
+    fc[i] <- unname(fore)
+    lwr[i] <- unname(PI[1])
+    upr[i] <- unname(PI[2])
+    ep.drawslist[[length(ep.drawslist)+1]] <- ep.draws
+    # Update newdata
+    if(maxlag > 0)
+    {
+      newdata[lagnames[seq(maxlag)]] <- c(fc[i], newdata[lagnames[1:(maxlag-1)]]) 
+    }
+    newdata[['trend']] <- trend[i+1]
+    if(i == max(seq_along(fc)))
+    {
+      newdata[['f1s_day']] <- 'NA'
+      newdata[['f1c_day']] <- 'NA'
+      newdata[['f2s_day']] <- 'NA'
+      newdata[['f2c_day']] <- 'NA'
+      newdata[['f3s_day']] <- 'NA'
+      newdata[['f3c_day']] <- 'NA'
+      newdata[['f1s_week']] <- 'NA'
+      newdata[['f1c_week']] <- 'NA'
+      newdata[['f2s_week']] <- 'NA'
+      newdata[['f2c_week']] <- 'NA'
+    }
+    else{
+      newdata[['f1s_day']] <- f1s_day[i+1]
+      newdata[['f1c_day']] <- f1c_day[i+1]
+      newdata[['f2s_day']] <- f2s_day[i+1]
+      newdata[['f2c_day']] <- f2c_day[i+1]
+      newdata[['f3s_day']] <- f3s_day[i+1]
+      newdata[['f3c_day']] <- f3c_day[i+1]
+      newdata[['f1s_week']] <- f1s_week[i+1]
+      newdata[['f1c_week']] <- f1c_week[i+1]
+      newdata[['f2s_week']] <- f2s_week[i+1]
+      newdata[['f2c_week']] <- f2c_week[i+1]
+    }
+  }
+  return(list(cbind.data.frame('fc' = fc, 'lwr' = lwr, 'upr' = upr), s.resid1, ep.drawslist))
+}
+
+
